93b6efa21abffd85fe4833bb41aaac5b140cbac1
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-04-21 16:51:27 -0300

    Criando método POST para criar novo TODO ao usuario

--- src/index.js
+++ src/index.js
@@ -49,6 +49,17 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { userMiddleware } = request;
+
+  const todo = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+  userMiddleware.todos.push(todo);
+  
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
